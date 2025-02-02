98ff232a91694a56e7a13701e8c958c5cb9e0c31
Jenny Mae A. Gabuya <jennymaegabuya4@gmail.com>
2025-02-02 18:24:35 +0800

    Add active JavaScript files for various plugins and components

---
+++ 404.php
@@ -0,0 +1,48 @@
+<!doctype html>
+<html class="no-js" lang="">
+
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="x-ua-compatible" content="ie=edge">
+  <title>Server Error 404</title>
+  <meta name="description" content="" />
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <link rel="shortcut icon" type="image/x-icon" href="img/favicon.ico" />
+  <link href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,700,900" rel="stylesheet" />
+  <link rel="stylesheet" href="css/bootstrap.min.css" />
+  <link rel="stylesheet" href="css/font-awesome.min.css" />
+  <link rel="stylesheet" href="css/owl.carousel.css" />
+  <link rel="stylesheet" href="css/owl.theme.css" />
+  <link rel="stylesheet" href="css/owl.transitions.css" />
+  <link rel="stylesheet" href="css/animate.css" />
+  <link rel="stylesheet" href="css/normalize.css" />
+  <link rel="stylesheet" href="css/meanmenu.min.css" />
+  <link rel="stylesheet" href="css/main.css" />
+  <link rel="stylesheet" href="css/educate-custon-icon.css" />
+  <link rel="stylesheet" href="css/morrisjs/morris.css" />
+  <link rel="stylesheet" href="css/scrollbar/jquery.mCustomScrollbar.min.css" />
+  <link rel="stylesheet" href="css/metisMenu/metisMenu.min.css" />
+  <link rel="stylesheet" href="css/metisMenu/metisMenu-vertical.css" />
+  <link rel="stylesheet" href="css/calendar/fullcalendar.min.css" />
+  <link rel="stylesheet" href="css/calendar/fullcalendar.print.min.css" />
+  <link rel="stylesheet" href="style.css" />
+  <link rel="stylesheet" href="css/responsive.css" />
+  <script src="js/vendor/modernizr-2.8.3.min.js"></script>
+</head>
+
+<body>
+  <div class="error-pagewrap">
+    <div class="error-page-int">
+      <div class="content-error">
+        <h1>ERROR <span class="counter"> 404</span></h1>
+        <p>Sorry, but the page you are looking for has note been found. Try checking the URL for the error, then hit the refresh button on your browser or try found something else in our app.</p>
+        <a href="index.php">Dashboard</a>
+      </div>
+      <div class="text-center login-footer">
+        <p>Copyright © 2025</p>
+      </div>
+    </div>
+  </div>
+</body>
+
+</html>
---
+++ 500.php
@@ -0,0 +1,48 @@
+<!doctype html>
+<html class="no-js" lang="">
+
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="x-ua-compatible" content="ie=edge">
+  <title>Server Error 500</title>
+  <meta name="description" content="" />
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <link rel="shortcut icon" type="image/x-icon" href="img/favicon.ico" />
+  <link href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,700,900" rel="stylesheet" />
+  <link rel="stylesheet" href="css/bootstrap.min.css" />
+  <link rel="stylesheet" href="css/font-awesome.min.css" />
+  <link rel="stylesheet" href="css/owl.carousel.css" />
+  <link rel="stylesheet" href="css/owl.theme.css" />
+  <link rel="stylesheet" href="css/owl.transitions.css" />
+  <link rel="stylesheet" href="css/animate.css" />
+  <link rel="stylesheet" href="css/normalize.css" />
+  <link rel="stylesheet" href="css/meanmenu.min.css" />
+  <link rel="stylesheet" href="css/main.css" />
+  <link rel="stylesheet" href="css/educate-custon-icon.css" />
+  <link rel="stylesheet" href="css/morrisjs/morris.css" />
+  <link rel="stylesheet" href="css/scrollbar/jquery.mCustomScrollbar.min.css" />
+  <link rel="stylesheet" href="css/metisMenu/metisMenu.min.css" />
+  <link rel="stylesheet" href="css/metisMenu/metisMenu-vertical.css" />
+  <link rel="stylesheet" href="css/calendar/fullcalendar.min.css" />
+  <link rel="stylesheet" href="css/calendar/fullcalendar.print.min.css" />
+  <link rel="stylesheet" href="style.css" />
+  <link rel="stylesheet" href="css/responsive.css" />
+  <script src="js/vendor/modernizr-2.8.3.min.js"></script>
+</head>
+
+<body>
+  <div class="error-pagewrap">
+    <div class="error-page-int">
+      <div class="content-error">
+        <h1>Server Error <span class="counter"> 500</span></h1>
+        <p>The server encountered something unexpected that didn't allow it to complete the request. We apologize.</p>
+        <a href="index.php">Dashboard</a>
+      </div>
+      <div class="text-center login-footer">
+        <p>Copyright © 2025</p>
+      </div>
+    </div>
+  </div>
+</body>
+
+</html>
