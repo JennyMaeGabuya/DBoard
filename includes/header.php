b9fcae88522699b929a4b4c5b8f0dff2767746a8
Jenny Mae A. Gabuya <jennymaegabuya4@gmail.com>
2025-02-17 13:26:11 +0800

    Update header and sidebar links to point to the dashboard page

--- includes/header.php
+++ includes/header.php
@@ -4,7 +4,7 @@
           <div class="row">
               <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
                   <div class="logo-pro">
-                      <a href="index.php"><img class="main-logo" src="img/logo/logo.png" alt="" /></a>
+                      <a href="./dashboard.php"><img class="main-logo" src="img/logo/logo.png" alt="" /></a>
                   </div>
               </div>
           </div>
--- includes/sidebar.php
+++ includes/sidebar.php
@@ -2,10 +2,10 @@
   <div class="left-sidebar-pro">
     <nav id="sidebar" class="">
       <div class="sidebar-header">
-        <a href="index.php">
+        <a href="./dashboard.php">
           <img class="main-logo" src="img/logo/header-logo.png" alt="" style="width: 200px; height: auto; margin-top: 10px; margin-bottom: 10px;" />
         </a>
-        <strong><a href="index.php"><img src="img/logo/sidebar-logo.png" alt="" style="width: 60px; height: auto;" /></a></strong>
+        <strong><a href="./dashboard.php"><img src="img/logo/sidebar-logo.png" alt="" style="width: 60px; height: auto;" /></a></strong>
       </div>
       <div class="left-custom-menu-adp-wrap comment-scrollbar">
         <nav class="sidebar-nav left-sidebar-menu-pro">
