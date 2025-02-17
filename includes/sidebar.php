bda1434f76ec46a56949f062be5712130b4bdd75
Jenny Mae A. Gabuya <jennymaegabuya4@gmail.com>
2025-02-17 13:17:12 +0800

    Add new logo images and update favicon across multiple pages

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -2,8 +2,10 @@
   <div class="left-sidebar-pro">
     <nav id="sidebar" class="">
       <div class="sidebar-header">
-        <a href="index.php"><img class="main-logo" src="img/logo/logo.png" alt="" /></a>
-        <strong><a href="index.php"><img src="img/logo/logosn.png" alt="" /></a></strong>
+        <a href="index.php">
+          <img class="main-logo" src="img/logo/header-logo.png" alt="" style="width: 200px; height: auto; margin-top: 10px; margin-bottom: 10px;" />
+        </a>
+        <strong><a href="index.php"><img src="img/logo/sidebar-logo.png" alt="" style="width: 60px; height: auto;" /></a></strong>
       </div>
       <div class="left-custom-menu-adp-wrap comment-scrollbar">
         <nav class="sidebar-nav left-sidebar-menu-pro">
