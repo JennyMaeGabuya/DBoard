3651b72f7c4045701edd32cedc407e9ab4734825
Jenny Mae A. Gabuya <jennymaegabuya4@gmail.com>
2025-02-17 12:32:29 +0800

    .html extesion to .php changes

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -2,8 +2,8 @@
   <div class="left-sidebar-pro">
     <nav id="sidebar" class="">
       <div class="sidebar-header">
-        <a href="index.html"><img class="main-logo" src="img/logo/logo.png" alt="" /></a>
-        <strong><a href="index.html"><img src="img/logo/logosn.png" alt="" /></a></strong>
+        <a href="index.php"><img class="main-logo" src="img/logo/logo.png" alt="" /></a>
+        <strong><a href="index.php"><img src="img/logo/logosn.png" alt="" /></a></strong>
       </div>
       <div class="left-custom-menu-adp-wrap comment-scrollbar">
         <nav class="sidebar-nav left-sidebar-menu-pro">
@@ -15,7 +15,7 @@
               </a>
             </li>
             <li>
-              <a title="Landing Page" href="events.html" aria-expanded="false"><span
+              <a title="Landing Page" href="events.php" aria-expanded="false"><span
                   class="educate-icon educate-event icon-wrap sub-icon-mg"
                   aria-hidden="true"></span>
                 <span class="mini-click-non">Event</span></a>
@@ -23,104 +23,104 @@
             <li>
               <a
                 class="has-arrow"
-                href="all-professors.html"
+                href="all-professors.php"
                 aria-expanded="false"><span
                   class="educate-icon educate-professor icon-wrap"></span>
                 <span class="mini-click-non">Professors</span></a>
               <ul class="submenu-angle" aria-expanded="false">
                 <li>
-                  <a title="All Professors" href="all-professors.html"><span class="mini-sub-pro">All Professors</span></a>
+                  <a title="All Professors" href="all-professors.php"><span class="mini-sub-pro">All Professors</span></a>
                 </li>
                 <li>
-                  <a title="Add Professor" href="add-professor.html"><span class="mini-sub-pro">Add Professor</span></a>
+                  <a title="Add Professor" href="add-professor.php"><span class="mini-sub-pro">Add Professor</span></a>
                 </li>
                 <li>
-                  <a title="Edit Professor" href="edit-professor.html"><span class="mini-sub-pro">Edit Professor</span></a>
+                  <a title="Edit Professor" href="edit-professor.php"><span class="mini-sub-pro">Edit Professor</span></a>
                 </li>
                 <li>
-                  <a title="Professor Profile" href="professor-profile.html"><span class="mini-sub-pro">Professor Profile</span></a>
+                  <a title="Professor Profile" href="professor-profile.php"><span class="mini-sub-pro">Professor Profile</span></a>
                 </li>
               </ul>
             </li>
             <li>
               <a
                 class="has-arrow"
-                href="all-students.html"
+                href="all-students.php"
                 aria-expanded="false"><span class="educate-icon educate-student icon-wrap"></span>
                 <span class="mini-click-non">Students</span></a>
               <ul class="submenu-angle" aria-expanded="false">
                 <li>
-                  <a title="All Students" href="all-students.html"><span class="mini-sub-pro">All Students</span></a>
+                  <a title="All Students" href="all-students.php"><span class="mini-sub-pro">All Students</span></a>
                 </li>
                 <li>
-                  <a title="Add Students" href="add-student.html"><span class="mini-sub-pro">Add Student</span></a>
+                  <a title="Add Students" href="add-student.php"><span class="mini-sub-pro">Add Student</span></a>
                 </li>
                 <li>
-                  <a title="Edit Students" href="edit-student.html"><span class="mini-sub-pro">Edit Student</span></a>
+                  <a title="Edit Students" href="edit-student.php"><span class="mini-sub-pro">Edit Student</span></a>
                 </li>
                 <li>
-                  <a title="Students Profile" href="student-profile.html"><span class="mini-sub-pro">Student Profile</span></a>
+                  <a title="Students Profile" href="student-profile.php"><span class="mini-sub-pro">Student Profile</span></a>
                 </li>
               </ul>
             </li>
             <li>
               <a
                 class="has-arrow"
-                href="all-courses.html"
+                href="all-courses.php"
                 aria-expanded="false"><span class="educate-icon educate-course icon-wrap"></span>
                 <span class="mini-click-non">Courses</span></a>
               <ul class="submenu-angle" aria-expanded="false">
                 <li>
-                  <a title="All Courses" href="all-courses.html"><span class="mini-sub-pro">All Courses</span></a>
+                  <a title="All Courses" href="all-courses.php"><span class="mini-sub-pro">All Courses</span></a>
                 </li>
                 <li>
-                  <a title="Add Courses" href="add-course.html"><span class="mini-sub-pro">Add Course</span></a>
+                  <a title="Add Courses" href="add-course.php"><span class="mini-sub-pro">Add Course</span></a>
                 </li>
                 <li>
-                  <a title="Edit Courses" href="edit-course.html"><span class="mini-sub-pro">Edit Course</span></a>
+                  <a title="Edit Courses" href="edit-course.php"><span class="mini-sub-pro">Edit Course</span></a>
                 </li>
                 <li>
-                  <a title="Courses Profile" href="course-info.html"><span class="mini-sub-pro">Courses Info</span></a>
+                  <a title="Courses Profile" href="course-info.php"><span class="mini-sub-pro">Courses Info</span></a>
                 </li>
                 <li>
-                  <a title="Product Payment" href="course-payment.html"><span class="mini-sub-pro">Courses Payment</span></a>
+                  <a title="Product Payment" href="course-payment.php"><span class="mini-sub-pro">Courses Payment</span></a>
                 </li>
               </ul>
             </li>
             <li>
               <a
                 class="has-arrow"
-                href="all-courses.html"
+                href="all-courses.php"
                 aria-expanded="false"><span class="educate-icon educate-library icon-wrap"></span>
                 <span class="mini-click-non">Library</span></a>
               <ul class="submenu-angle" aria-expanded="false">
                 <li>
-                  <a title="All Library" href="library-assets.html"><span class="mini-sub-pro">Library Assets</span></a>
+                  <a title="All Library" href="library-assets.php"><span class="mini-sub-pro">Library Assets</span></a>
                 </li>
                 <li>
-                  <a title="Add Library" href="add-library-assets.html"><span class="mini-sub-pro">Add Library Asset</span></a>
+                  <a title="Add Library" href="add-library-assets.php"><span class="mini-sub-pro">Add Library Asset</span></a>
                 </li>
                 <li>
-                  <a title="Edit Library" href="edit-library-assets.html"><span class="mini-sub-pro">Edit Library Asset</span></a>
+                  <a title="Edit Library" href="edit-library-assets.php"><span class="mini-sub-pro">Edit Library Asset</span></a>
                 </li>
               </ul>
             </li>
             <li>
               <a
                 class="has-arrow"
-                href="all-courses.html"
+                href="all-courses.php"
                 aria-expanded="false"><span
                   class="educate-icon educate-department icon-wrap"></span>
                 <span class="mini-click-non">Departments</span></a>
               <ul class="submenu-angle" aria-expanded="false">
                 <li>
-                  <a title="Departments List" href="departments.html"><span class="mini-sub-pro">Departments List</span></a>
+                  <a title="Departments List" href="departments.php"><span class="mini-sub-pro">Departments List</span></a>
                 </li>
                 <li>
-                  <a title="Add Departments" href="add-department.html"><span class="mini-sub-pro">Add Departments</span></a>
+                  <a title="Add Departments" href="add-department.php"><span class="mini-sub-pro">Add Departments</span></a>
                 </li>
                 <li>
-                  <a title="Edit Departments" href="edit-department.html"><span class="mini-sub-pro">Edit Departments</span></a>
+                  <a title="Edit Departments" href="edit-department.php"><span class="mini-sub-pro">Edit Departments</span></a>
                 </li>
               </ul>
             </li>
@@ -129,22 +129,22 @@
                 <span class="mini-click-non">Pages</span></a>
               <ul class="submenu-angle page-mini-nb-dp" aria-expanded="false">
                 <li>
-                  <a title="Login" href="login.html"><span class="mini-sub-pro">Login</span></a>
+                  <a title="Login" href="login.php"><span class="mini-sub-pro">Login</span></a>
                 </li>
                 <li>
-                  <a title="Register" href="register.html"><span class="mini-sub-pro">Register</span></a>
+                  <a title="Register" href="register.php"><span class="mini-sub-pro">Register</span></a>
                 </li>
                 <li>
-                  <a title="Lock" href="lock.html"><span class="mini-sub-pro">Lock</span></a>
+                  <a title="Lock" href="lock.php"><span class="mini-sub-pro">Lock</span></a>
                 </li>
                 <li>
-                  <a title="Password Recovery" href="password-recovery.html"><span class="mini-sub-pro">Password Recovery</span></a>
+                  <a title="Password Recovery" href="password-recovery.php"><span class="mini-sub-pro">Password Recovery</span></a>
                 </li>
                 <li>
-                  <a title="404 Page" href="404.html"><span class="mini-sub-pro">404 Page</span></a>
+                  <a title="404 Page" href="404.php"><span class="mini-sub-pro">404 Page</span></a>
                 </li>
                 <li>
-                  <a title="500 Page" href="500.html"><span class="mini-sub-pro">500 Page</span></a>
+                  <a title="500 Page" href="500.php"><span class="mini-sub-pro">500 Page</span></a>
                 </li>
               </ul>
             </li>
