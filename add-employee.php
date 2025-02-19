7c317e7475a891a94d356c002fee00905ae46d64
erikamagnaye <147479942+eka31@users.noreply.github.com>
2025-02-19 15:37:28 +0800

    Update add-employee.php

--- add-employee.php
+++ add-employee.php
@@ -102,14 +102,8 @@ include "dbcon.php";
                   <div class="row">
                     <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
                       <div class="review-content-section">
-<<<<<<< Updated upstream
                         <form id="add-department" action="basic-info.php" method="POST" enctype="multipart/form-data" class="add-department">
                           <div class="row">
-=======
-                        <form id="add-department"    action="basic-info.php" method="POST" enctype="multipart/form-data"  class="add-department">
-                        <div class="row">
-                       
->>>>>>> Stashed changes
                             <div
                               class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
                               <div class="form-group">
