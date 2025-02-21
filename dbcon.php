750252976cdeb298101f40deca8b067dd84c6476
Jenny Mae A. Gabuya <jennymaegabuya4@gmail.com>
2025-02-21 18:41:03 +0800

    Update dbcon.php

--- dbcon.php
+++ dbcon.php
@@ -1,13 +1,13 @@
 <?php
-$servername = "localhost"; // e.g., "localhost"
+$servername = "localhost";
 $username = "root";
 $password = "";
 $database = "hr_records";
 
-$conn = new mysqli($servername, $username, $password, $database);
+$con = new mysqli($servername, $username, $password, $database);
 
 // Check the connection
-if ($conn->connect_error) {
+if ($con->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 ?>
