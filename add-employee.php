02d7754c1d006f59af8ae48db9e0c3ada018b082
Everson D. Dimaculangan <142397050+EversonDimaculangan05@users.noreply.github.com>
2025-02-26 10:53:47 +0800

    Change Icon the Home,Employees, and Add new Employee

--- add-employee.php
+++ add-employee.php
@@ -63,13 +63,20 @@ include "dbcon.php";
                     <div class="col-lg-12">
                       <ul class="breadcome-menu" style="display: flex; justify-content: flex-start; padding-left: 0; padding: 0;">
                         <li>
-                          <a href="dashboard.php">
-                            <i class="fa fa-home"></i> Home
-                          </a>
-                          <span class="bread-slash">/</span>
-                          <a href="all-employees.php"> Employees</a>
-                          <span class="bread-slash">/</span>
-                          <span class="bread-blod">Add New Employee</span>
+                        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+
+<a href="dashboard.php">
+    <i class="fas fa-home"></i> <strong>Home</strong>
+</a>
+<span class="bread-slash"> / </span>
+<a href="all-employees.php">
+    <i class="fas fa-users"></i> <strong>Employees</strong>
+</a>
+<span class="bread-slash"> / </span>
+<span class="bread-blod">
+    <i class="fas fa-user-plus"></i> <strong>Add New Employee</strong>
+</span>
+
                         </li>
                       </ul>
                     </div>
