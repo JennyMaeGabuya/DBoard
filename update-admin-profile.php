55acf1e616bb2a07b9caad4b515163a77b6d1dfe
Jelladane Leones Peloramas <jelladanepeloramas07@gmail.com>
2025-03-03 16:02:01 +0800

    not yet finished (2 column) push ko lang at nalilito na akis

--- update-admin-profile.php
+++ update-admin-profile.php
@@ -64,7 +64,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         }
 
         // Bind parameters
-        $stmt->bind_param("sssssssssssss", 
+        $stmt->bind_param("ssssssssssssss", 
             $firstname, $middlename, $lastname, $name_extension,
             $email_address, $mobile_no, $dob, $address,
             $pob, $civil_status, $sex, $blood_type,
