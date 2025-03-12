06e30b1351bda3d7722545c49e20d7a571ee2c1a
Jenny Mae A. Gabuya <jennymaegabuya4@gmail.com>
2025-03-12 13:49:25 +0800

    NEW DB

--- database/hr_records (11).sql
+++ database/hr_records (11).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Mar 11, 2025 at 09:10 AM
+-- Generation Time: Mar 12, 2025 at 05:47 AM
 -- Server version: 8.3.0
 -- PHP Version: 8.2.18
 
@@ -85,40 +85,7 @@ CREATE TABLE IF NOT EXISTS `appointed_cert_issuance` (
 
 INSERT INTO `appointed_cert_issuance` (`id`, `fullname`, `lastname`, `sex`, `start_date`, `position`, `office_appointed`, `salary`, `pera`, `rta`, `clothing`, `mid_year_bonus`, `year_end_bonus`, `cash_gift`, `productivity_enhancement`, `date_issued`, `created_at`, `updated_at`) VALUES
 (3, 'Hon. Atty. Juan Dela Cruz', 'Dela Cruz', 'Male', '2025-02-01', 'Admin Officer II', 'Budget Office', 12500.00, 5600.00, 534534.00, 543543.00, 45345.00, 4345435.00, 4345435.00, 543543.00, '2025-03-07', '2025-03-08 08:05:23', '2025-03-08 08:35:00'),
-(4, 'Appointed test huhu', 'Test q ko', 'Male', '2024-12-07', 'Admin Officer 321', 'Assesor\'s Office', 56878.00, 987.00, 6578.00, 788.00, 879.00, 796.00, 5666.00, 7769.00, '2025-03-08', '2025-03-08 08:43:48', '2025-03-08 08:45:09');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `compensation`
---
-
-DROP TABLE IF EXISTS `compensation`;
-CREATE TABLE IF NOT EXISTS `compensation` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `employee_no` varchar(20) COLLATE utf8mb4_general_ci NOT NULL,
-  `salary` decimal(10,2) NOT NULL,
-  `pera` decimal(10,2) NOT NULL,
-  `rt_allowance` decimal(10,2) NOT NULL,
-  `allowance` decimal(10,2) NOT NULL,
-  `clothing` decimal(10,2) NOT NULL,
-  `mid_year` decimal(10,2) NOT NULL,
-  `year_end_bonus` decimal(10,2) NOT NULL,
-  `cash_gift` decimal(10,2) NOT NULL,
-  `productivity_incentive` decimal(10,2) NOT NULL,
-  `issued_date` date NOT NULL,
-  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`),
-  KEY `employee_no` (`employee_no`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `compensation`
---
-
-INSERT INTO `compensation` (`id`, `employee_no`, `salary`, `pera`, `rt_allowance`, `allowance`, `clothing`, `mid_year`, `year_end_bonus`, `cash_gift`, `productivity_incentive`, `issued_date`, `created_at`, `updated_at`) VALUES
-(1, 'HRM-ADMIN', 545345.00, 435435.00, 443543.00, 435435.00, 43543543.00, 5435435.00, 99999999.99, 99999999.99, 5435435.00, '2025-03-01', '2025-02-28 16:00:00', '2025-02-28 16:00:00');
+(4, 'Appointed test huhu', 'Test q ko', 'Male', '2024-12-07', 'Admin Officer 321', 'Assesor\'s Office', 56878.00, 987.00, 6578.00, 788.00, 879.00, 796.00, 5666.00, 7769.00, '2025-03-12', '2025-03-08 08:43:48', '2025-03-12 01:12:15');
 
 -- --------------------------------------------------------
 
@@ -171,7 +138,7 @@ CREATE TABLE IF NOT EXISTS `employee` (
   `dob` date NOT NULL,
   `pob` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
   `sex` enum('Male','Female') COLLATE utf8mb4_general_ci NOT NULL,
-  `civil_status` enum('Single','Married','Widowed','Separated','Others') COLLATE utf8mb4_general_ci NOT NULL,
+  `civil_status` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
   `address` text COLLATE utf8mb4_general_ci NOT NULL,
   `blood_type` enum('A+','A-','B+','B-','AB+','AB-','O+','O-','Unknown') COLLATE utf8mb4_general_ci DEFAULT 'Unknown',
   `mobile_no` bigint DEFAULT NULL,
@@ -188,6 +155,7 @@ CREATE TABLE IF NOT EXISTS `employee` (
 --
 
 INSERT INTO `employee` (`employee_no`, `firstname`, `middlename`, `lastname`, `name_extension`, `dob`, `pob`, `sex`, `civil_status`, `address`, `blood_type`, `mobile_no`, `email_address`, `image`, `created_at`, `updated_at`) VALUES
+('dsd', 'dsd', 'dsd', 'dsd', 'dsd', '2025-03-12', 'samin', 'Female', '', 'lipa', 'AB-', 55657663354, 'ds@gmail.com', NULL, '2025-03-11 16:00:00', '2025-03-11 16:00:00'),
 ('EMP001', 'Juan', 'Dela', 'Cruz', 'Jr.', '1985-03-25', 'Manila', 'Male', 'Married', '123 Mabini St., Manila', 'O+', 9171234567, 'juan.cruz@email.com', NULL, '2025-02-19 14:09:03', '2025-02-19 14:14:08'),
 ('EMP002', 'Noime', 'T.', 'Tipan', '', '1990-07-12', 'Quezon City', 'Female', 'Married', '456 Rizal Ave., QC', 'A+', 9281234567, 'maria.lopez@email.com', 'Noims.png', '2025-02-19 14:09:03', '2025-03-06 04:35:30'),
 ('EMP003', 'Gelyn', 'M.', 'Katimbang', NULL, '1988-05-18', 'Cebu City', 'Female', 'Married', '789 Osmena Blvd., Cebu', 'B+', 9391234567, 'pedro.gonzalez@email.com', 'Gelyn.png', '2025-02-19 14:09:03', '2025-03-06 04:37:19'),
@@ -198,7 +166,9 @@ INSERT INTO `employee` (`employee_no`, `firstname`, `middlename`, `lastname`, `n
 ('EMP008', 'Isabel', 'T.', 'Mendoza', '', '1998-02-25', 'Laguna', 'Female', 'Single', '505 Calamba Rd., Laguna', 'AB-', 9891234567, 'isabel.mendoza@email.com', NULL, '2025-02-19 14:09:03', '2025-02-19 14:14:28'),
 ('EMP009', 'Miguel', 'R.', 'Domingo', '', '1991-08-09', 'Pampanga', 'Male', 'Married', '606 Angeles St., Pampanga', 'O+', 9991234567, 'miguel.domingo@email.com', NULL, '2025-02-19 14:09:03', '2025-02-19 14:14:31'),
 ('EMP010', 'Janet', 'M.', 'Ilagan', '', '1984-12-01', 'Zamboanga City', 'Female', 'Married', '707 Pilar St., Zamboanga', 'A+', 9091234567, 'carmen.reyes@email.com', 'Janet.png', '2025-02-19 14:09:03', '2025-03-06 04:35:03'),
-('HRM-ADMIN', 'Gally', 'Dimayuga', 'Tipan', '', '1990-06-28', 'CUENCA, BATANGAS', 'Male', 'Single', 'CUENCA, BATANGAS', 'A+', 9123456789, 'admin@gmail.com', 'Gally.png', '2025-02-17 06:33:54', '2025-03-06 04:35:15');
+('HRM-ADMIN', 'Gally', 'Dimayuga', 'Tipan', '', '1990-06-28', 'CUENCA, BATANGAS', 'Male', 'Single', 'CUENCA, BATANGAS', 'A+', 9123456789, 'admin@gmail.com', 'Gally.png', '2025-02-17 06:33:54', '2025-03-12 01:06:17'),
+('sasa', 'sas', 'sas', 'sasa', 'sa', '2025-03-12', 'sasa', 'Male', 'Other', 'sas', 'A+', 34983490832, 'sas@gmail.com', '1x1-edited.jpg', '2025-03-11 16:00:00', '2025-03-11 16:00:00'),
+('sdas', 'dsad', 'dasdsa', 'dasdsad', 'dsads', '2025-03-12', 'hehehe', 'Male', 'sample new civil status', 'samin', 'A-', 32483094823, 'asd@gmail.com', '404 not found.jpg', '2025-03-11 16:00:00', '2025-03-11 16:00:00');
 
 -- --------------------------------------------------------
 
@@ -214,8 +184,7 @@ CREATE TABLE IF NOT EXISTS `events` (
   `end_date` datetime NOT NULL,
   `description` text,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `events`
@@ -248,14 +217,17 @@ CREATE TABLE IF NOT EXISTS `government_info` (
   UNIQUE KEY `sss_no` (`sss_no`),
   UNIQUE KEY `tin_no` (`tin_no`),
   KEY `employee_no` (`employee_no`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `government_info`
 --
 
 INSERT INTO `government_info` (`id`, `employee_no`, `gsis_no`, `pag_ibig_no`, `philhealth_no`, `sss_no`, `tin_no`, `created_at`, `updated_at`) VALUES
-(2, 'HRM-ADMIN', '4332423-3423', '3287-387245', '94586-7863', '4353-2435', '325436-32422', '2025-03-11 08:25:50', '2025-03-11 08:25:50');
+(2, 'HRM-ADMIN', '4332423-3423', '3287-387245', '94586-7863', '4353-2435', '325436-32422', '2025-03-11 08:25:50', '2025-03-11 08:25:50'),
+(3, 'dsd', '1321s', 'dsd232', 'asad23213', '23221dsad', 'sdqwe2', '2025-03-11 16:00:00', '2025-03-11 16:00:00'),
+(4, 'sasa', '3312', '23213', '45435', '4543', '7868', '2025-03-11 16:00:00', '2025-03-11 16:00:00'),
+(5, 'sdas', '23123sds', '4234dfd', '7567fgf', '2324fddf', '9797cvc', '2025-03-11 16:00:00', '2025-03-11 16:00:00');
 
 -- --------------------------------------------------------
 
@@ -319,11 +291,11 @@ CREATE TABLE IF NOT EXISTS `service_records` (
 
 INSERT INTO `service_records` (`id`, `employee_no`, `from_date`, `to_date`, `designation`, `status`, `salary`, `station_place`, `branch`, `abs_wo_pay`, `date_separated`, `cause_of_separation`, `created_at`, `updated_at`) VALUES
 (2, 'HRM-ADMIN', '2025-03-05', '2025-03-07', 'fdf', 'fdf', 3232.00, 'HRM', 'M-Kahoy', 'NA', '2025-03-11', 'NA', '2025-03-04 16:00:00', '2025-03-05 03:48:06'),
-(3, 'HRM-ADMIN', '2024-03-08', '2025-03-06', 'Office of the HRM', 'Regular', 12500.00, 'HRMO', 'MKahoy', '--td--', '2025-03-07', 'NA', '2025-03-07 16:00:00', '2025-03-07 16:00:00'),
+(3, 'HRM-ADMIN', '2024-03-08', '2025-03-06', 'fdf', 'Regular', 12500.00, 'HRM', 'MKahoy', '--td--', '2025-03-07', 'NA', '2025-03-07 16:00:00', '2025-03-12 01:05:40'),
 (4, 'EMP009', '2025-03-08', '2025-04-01', 'sa', 'sa', 1234345.00, 'sdsd', 'dsds', 'dsd', '2025-03-07', 'dsds', '2025-03-07 16:00:00', '2025-03-07 16:00:00'),
 (5, 'EMP009', '2025-03-07', '2025-03-04', 'dsd', 'ddsd', 232323.00, 'dsd', 'dsd', 'sdds', '2025-03-04', 'as', '2025-03-07 16:00:00', '2025-03-07 16:00:00'),
-(6, 'HRM-ADMIN', '2025-03-11', '2025-03-12', 'testing heheh', 'sdjhasjd', 38973487.00, 'saming office', 'hsdjah', '--td--', '2025-03-10', 'NA', '2025-03-10 16:00:00', '0000-00-00 00:00:00'),
-(7, 'HRM-ADMIN', '2025-03-11', '2025-03-12', 'sa', 'sas', 45345.00, '63dfg', '6gdf', '546', '2025-03-12', 'hehehhhe', '2025-03-10 16:00:00', '0000-00-00 00:00:00');
+(6, 'HRM-ADMIN', '2025-03-11', '2025-03-12', 'fdf', 'sdjhasjd', 38973487.00, 'HRM', 'hsdjah', '--td--', '2025-03-10', 'NA', '2025-03-10 16:00:00', '2025-03-12 01:05:40'),
+(7, 'HRM-ADMIN', '2025-03-11', '2025-03-12', 'fdf', 'sas', 45345.00, 'HRM', '6gdf', '546', '2025-03-12', 'hehehhhe', '2025-03-10 16:00:00', '2025-03-12 01:05:40');
 
 --
 -- Constraints for dumped tables
@@ -334,12 +306,6 @@ INSERT INTO `service_records` (`id`, `employee_no`, `from_date`, `to_date`, `des
 --
 ALTER TABLE `admin`
   ADD CONSTRAINT `admin_ibfk_1` FOREIGN KEY (`employee_no`) REFERENCES `employee` (`employee_no`) ON DELETE CASCADE ON UPDATE CASCADE;
-
---
--- Constraints for table `compensation`
---
-ALTER TABLE `compensation`
-  ADD CONSTRAINT `compensation_ibfk_1` FOREIGN KEY (`employee_no`) REFERENCES `employee` (`employee_no`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints for table `government_info`
